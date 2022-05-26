41ac62b4759075b37aaebb79aafc0f9cf0b3c515
Berk <70212751+BerkTas20@users.noreply.github.com>
2022-05-26 11:00:16 +0300

    Initial commit

---
+++ src/homeWork/PatikaHomework1.java
@@ -0,0 +1,10 @@
+package homeWork;
+
+public class PatikaHomework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/homeWork/PatikaHomework3.java
@@ -0,0 +1,10 @@
+package homeWork;
+
+public class PatikaHomework3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
